f128373465eb4495bdcbb5c55f1d34fb1a3f3bdc
OlhaMal <117120298+OlhaMal@users.noreply.github.com>
2024-04-22 06:10:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const sideMenuToggle = document.getElementById('side-menu__toggle');
-      if (sideMenuToggle) {
-      sideMenuToggle.addEventListener('change', function() {
-        const body = document.body;
-        if (sideMenuToggle.checked) {
-          body.classList.add('no-scroll');
-        } else {
-          body.classList.remove('no-scroll');
-        }
-      });
+  const sideMenuToggle = document.getElementById('side-menu__toggle');
+  const body = document.body;
+
+  function updateBodyScroll() {
+    if (sideMenuToggle && !window.matchMedia('(min-width: 1280px)').matches) {
+      if (sideMenuToggle.checked) {
+        body.classList.add('no-scroll');
+      } else {
+        body.classList.remove('no-scroll');
+      }
+    } else {
+      body.classList.remove('no-scroll');
     }
-  });
+  }
+
+  updateBodyScroll();
+
+  if (sideMenuToggle) {
+    sideMenuToggle.addEventListener('change', updateBodyScroll);
+  }
+
+  window.addEventListener('resize', updateBodyScroll);
+});
